c9e25df9c255bd45b54f69928b9b6278d395e52b
dominik-ernst <dominik.ernst@tu-berlin.de>
2017-11-07 11:05:27 +0100

    added response entity wrapper

--- src/main/java/de/tub/ise/anwsys/controllers/HelloController.java
+++ src/main/java/de/tub/ise/anwsys/controllers/HelloController.java
@@ -3,6 +3,7 @@ package de.tub.ise.anwsys.controllers;
 import de.tub.ise.anwsys.models.User;
 import de.tub.ise.anwsys.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import java.util.Optional;
 @RestController
 public class HelloController {
 
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Autowired
     public HelloController(UserRepository userRepository) {
@@ -21,15 +22,15 @@ public class HelloController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String answerAndRegister(@RequestParam(value = "user", defaultValue = "AnwSys Student") String username) {
+    public ResponseEntity<?> answerAndRegister(@RequestParam(value = "user", defaultValue = "AnwSys Student") String username) {
         Optional<User> foundUser = userRepository.findByName(username);
 
         if (foundUser.isPresent()) {
-            return String.format("Welcome back, %s. How are your studies going?", foundUser.get().getName());
+            return ResponseEntity.ok(String.format("Welcome back, %s. How are your studies going?", foundUser.get().getName()));
         } else {
             User u = new User(username);
             userRepository.save(u);
-            return String.format("Welcome new User %s!", u.getName());
+            return ResponseEntity.ok(String.format("Welcome new User %s!", u.getName()));
         }
     }
 }
